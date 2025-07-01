664f8b3721eb07430b2d80c17e2c6878df1231c1
Aashi Goel <aashi25goel@gmail.com>
2025-07-01 18:00:50 +0530

    sum of 2 numbers

---
+++ functions/src/Sum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        sum(); //calling a function in main function
+    }
+    static void sum(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter number 1:- ");
+        int num1 = in.nextInt();
+        System.out.println("Enter number 2 :- ");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The sum is " + sum);
+    }
+}
