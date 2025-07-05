ffe391b3dee3547850729891a0e7a37c706e196f
Aashi Goel <aashi25goel@gmail.com>
2025-07-05 13:50:40 +0530

    pass by reference and value functions added

---
+++ functions/src/PassingExample.java
@@ -0,0 +1,9 @@
+public class PassingExample {
+    public static void main(String[] args) {
+        String name = "Aashi Goel";
+        greet(name);
+    }
+    static void greet(String n){
+        System.out.println(n);
+     }
+}
