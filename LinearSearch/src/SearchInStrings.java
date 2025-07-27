85728997400ef9ec8418a9d34ac18a84844b2981
Aashi Goel <aashi25goel@gmail.com>
2025-07-27 21:20:57 +0530

    searching in strings done

---
+++ LinearSearch/src/SearchInStrings.java
@@ -0,0 +1,18 @@
+public class SearchInStrings {
+    public static void main(String[] args) {
+        char target = 'a';
+        boolean ans = search("name", target);
+        System.out.println(ans);
+    }
+    static boolean search(String str , char target){
+        if (str.isEmpty()){
+            return false;
+        }
+        for (int i=0 ; i<str.length() ; i++){
+            if (target == str.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
