7c1c9d975c50db01897c38869c9b412052c53ada
Aashi Goel <aashi25goel@gmail.com>
2025-07-27 18:05:48 +0530

    linear search

---
+++ LinearSearch/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {23 , 45 , 1 , 2 , 8 , 19 , -3 , 16 , -11 , 28};
+        int target = 11;
+        int ans = linearsearch(nums , target);
+        System.out.println(ans);
+    }
+
+    //search in the array: return the index if item found
+    //otherwise if item not found return -1
+
+    static int linearsearch(int[] arr, int target){
+        if (arr.length == 0){
+            return -1;
+        }
+
+        //run a for loop
+        for (int index=0 ; index<arr.length ; index++){
+            int element = arr[index];
+            if (element == target){
+                return index;
+            }
+        }
+        return -1;
+    }
+}
