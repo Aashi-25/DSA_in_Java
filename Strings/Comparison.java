d3ec5d0536720fb87947bc509da6cf05ef61bec5
Aashi Goel <aashi25goel@gmail.com>
2025-12-07 21:31:48 +0530

    strings in java

---
+++ Strings/Comparison.java
@@ -0,0 +1,16 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Comparison {
+    public static void main(String[] args) {
+        String a = "AG";
+        String b = "AG";
+        System.out.println(a==b);
+
+        String name1 = new String("AG");
+        String name2 = new String("AG");
+        System.out.println(name1 == name2);
+
+        //if you only wish to compare values and don't care whether the ref variable is pointing to the same object or not -> use equals() method
+        System.out.println(name1.equals(name2));
+     }
+}
