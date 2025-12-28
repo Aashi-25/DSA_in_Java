dc5c318408e5bd659f90d546260bc34e49d4f541
Aashi Goel <aashi25goel@gmail.com>
2025-12-28 19:49:29 +0530

    Recursion basics

---
+++ Recursion/src/NumsExample.java
@@ -0,0 +1,29 @@
+public class NumsExample {
+    public static void main(String[] args) {
+        //write a function that takes in a number and prints it
+        //print first 5 numbers
+        print1(1);
+    }
+
+    //function calling another function
+    //body and parameters are same for all the functions..the structure of it
+    static void print1(int n){
+        System.out.println(n);
+        print2(2);
+    }
+    static void print2(int n){
+        System.out.println(n);
+        print3(3);
+    }
+    static void print3(int n){
+        System.out.println(n);
+        print4(4);
+    }
+    static void print4(int n){
+        System.out.println(n);
+        print5(5);
+    }
+    static void print5(int n){
+        System.out.println(n);
+    }
+}
