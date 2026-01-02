fa7e94e0648330f94e3af670245ab8460ff11312
Aashi Goel <aashi25goel@gmail.com>
2026-01-02 12:59:37 +0530

    find unique among all duplicates in an array

---
+++ MathForDSA/src/FindUnique.java
@@ -0,0 +1,13 @@
+public class FindUnique {
+    public static void main(String[] args) {
+        int[] arr = {2 , 3  , 4 ,3 , 2 , 4 , 6};
+        System.out.println(FindUnique(arr));
+    }
+    static int FindUnique(int[] arr){
+        int unique = 0;
+        for(int n:arr){
+            unique ^= n;
+        }
+        return unique;
+    }
+}
