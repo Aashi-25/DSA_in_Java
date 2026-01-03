a63789148619cf3d6116f00b545ac9e28d3a2ffb
Aashi Goel <aashi25goel@gmail.com>
2026-01-03 11:43:15 +0530

    magic num

---
+++ MathForDSA/src/MagicNumber.java
@@ -0,0 +1,14 @@
+public class MagicNumber {
+    public static void main(String[] args) {
+        int n = 2;
+        int magic = 0;
+        int base = 5;
+        while(n > 0){
+            int last = n & 1;
+            magic += last*base;
+            n = n>>1;
+            base *= 5;
+        }
+        System.out.println(magic);
+    }
+}
